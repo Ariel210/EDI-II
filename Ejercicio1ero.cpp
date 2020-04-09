54dcd2ebb66b804751a0a7289ac555e86109a23f
Ariel Izurieta <ariel_izurieta@hotmail.com>
2020-04-09 14:13:26 -0300

    Ejercicio1

---
+++ Ejercicio1ero.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int main () 
+{
+	string nombre = "Ariel";
+	
+	cout<<"Digite su nombre: ";
+	cin>>nombre;
+
+	cout<<"\nBienvenido " + nombre + " a EDI 2 de 2do." + " Saludos Profe.";
+	
+	return 0;
+	
+}
